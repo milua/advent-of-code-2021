458fb53d6e487ea973249fe752190a4a8eb93148
Isabella Tran <isabella.tran@wps.de>
2021-12-02 09:31:40 +0100

    implement day two of adventofcode

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,12 @@ public class Main {
 
     public static void main(String[] args)  //static method
     {
-        System.out.println("Count of all increases: " + DayOne.countAllMeasurementIncreases());
+        // System.out.println("Count of all increases: " + DayOne.countAllMeasurementIncreases());
+
+        var dayTwo = DayTwo.calculatePosition();
+        System.out.println("Position X: " + dayTwo[0] + ", Position Y: " + dayTwo[1]);
+        System.out.println("Multiplied together: " + dayTwo[0] * dayTwo[1]);
+
     }
 }
 
